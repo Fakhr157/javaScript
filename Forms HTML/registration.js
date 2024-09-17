9cdfa8706ae0b9f8c2b719690d90e91da0c9c9f6
yasir.alam <yasir.alam@HW-PD-08>
2024-09-18 02:36:24 +0500

    forms

---
+++ Forms HTML/registration.js
@@ -0,0 +1,37 @@
+
+var form = document.getElementById("form");
+
+
+
+function onSubmit(event) {
+    event.preventDefault();
+    let myData = {};
+    myData.firstname = document.getElementById("first").value;
+    myData.lastname = document.getElementById("last").value;
+    myData.address = document.getElementById("address").value;
+    myData.conatct = document.getElementById("conatct").value;
+    myData.email = document.getElementById("email").value;
+    myData.password = document.getElementById("password").value;
+    myData.confirmPassword = document.getElementById("confirmPassword").value;
+    myData.english = document.getElementById("english").checked;
+    myData.pushto = document.getElementById("urdu").checked;
+    myData.urdu = document.getElementById("pushto").checked;
+    myData.male = document.getElementById("male").checked;
+    myData.female = document.getElementById("female").checked;
+
+    myData.dropdown = document.getElementById("dropdown").value;
+    console.log(myData);
+
+}
+
+function resetsss(){
+    document.getElementById("first").value = "";
+    document.getElementById("last").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("conatct").value = "";
+    document.getElementById("email").value = "";
+}
+
+
+// let fakhir ={name: "Fakhir", age:20 , email: "fakhir@gmail.com" , gender: "male"}
+// console.log(fakhir)
