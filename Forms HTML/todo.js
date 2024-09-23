20ede35e9deaad10332b2349111591ab4c3bea2d
Yasir <fakhiralam2019@gmail.com>
2024-09-23 21:45:35 +0500

    to do list

--- Forms HTML/todo.js
+++ Forms HTML/todo.js
@@ -1,12 +1,15 @@
-function todo(){
-    let ul = document.getElementById("todo");
-    let input = document.getElementById("firstname");
-    let li = document.createElement("input");
-    // li.appendChild(document.createTextNode(input));
-    li.value = input.value
-    ul.appendChild(li);
-    input.value='';
-    //document.getElementById("input").value;
+function addName() {
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  const fullName = `${firsName} $ {LastName}`;
+  const li = document.createElement("li");
+  li.textcontent = fullName;
+  document.getElementById("nameList").appendChild(li);
+  // let input = document.getElementById("firstname");
+  // let li = document.createElement("input");
+  // li.appendChild(document.createTextNode(input));
+  // li.value = input.value
+  // ul.appendChild(li);
+  // input.value='';
+  // //document.getElementById("input").value;
 }
-
-
