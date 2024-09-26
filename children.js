0170b9d2551d95157322ff55717c6dfca8fa3898
Yasir <fakhiralam2019@gmail.com>
2024-09-26 21:41:54 +0500

    abc

--- children.js
+++ children.js
@@ -1,6 +1,14 @@
+function changeabc() {
+  var element = document.getElementById("Provident");
+  var elementfirstChild = element.children[0];
+  elementfirstChild.setAttribute("src", "image/1.jpg");
+  console.log(elementfirstChild);
 
-function changeabc(){
-var element = document.getElementById("Provident").children;
-element[0].setAttribute('src','image/1.jpg')
-element[1].childNodes[5].innerText=("Eassy Pyments")
+  var elementsecChild = element.children[1];
+
+  //   element.children[1].childNodes[5].innerText="Eassy Pyments";
+  //   console.log(element.children[1].children);
+  var description = elementsecChild.getElementsByClassName("desc")[0];
+  description.innerText = "Eassy Pyments";
+  console.log(description);
 }
--- table.js
+++ table.js
@@ -11,19 +11,22 @@
 // }
 
 function secondtable(){
-    console.log("function call")
-var parentelement = document.getElementById("secondtable")
-var childelement = document.createElement("tr");
-
-var name = document.getElementById("name");
-var age = document.getElementById("age");
-var address = document.getElementById("address");
-var  city = document.getElementById("city")
-
-childelement.innerHTML = "<td>"+ name.value +"</td><td>"+ age.value+"</td><td>" + address.value +"</td><td>" + city.value + "</td>";
-parentelement.appendChild(childelement)
+    var parentelement = document.getElementById("secondtable");
+    
+    // var tBody = parentelement.getElementsByTagName('tbody')[0]
+    var childelement = document.createElement("tr");
+    
+    var name = document.getElementById("name");
+    var age = document.getElementById("age");
+    var address = document.getElementById("address");
+    var  city = document.getElementById("city")
+    
+    childelement.innerHTML = "<td>"+ name.value +"</td><td>"+ age.value+"</td><td>" + address.value +"</td><td>" + city.value + "</td>";
+    console.log(parentelement);
+    // console.log(tBody);
+    parentelement.appendChild(childelement)
 name.value = "";
 age.value = "";
-address.value = "";
 city.value = "";
+address.value = "";
 }
